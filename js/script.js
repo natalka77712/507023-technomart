19f47c519ca47279046fb7018205df06421d87c7
natalka77712 <kostousik@mail.ru>
2018-07-08 13:57:09 +0300

    финальная проверка

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
  var feedback = document.querySelector(".write-us");
+ var popup = document.querySelector(".contact-us");
 
 
- feedback.addEventListener("click", function () {
+
+ feedback.addEventListener("click", function (evt) {
+ 	evt.preventDefault();
   console.log("Клик по кнопке обратной связи");
  
